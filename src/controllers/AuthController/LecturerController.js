8ad778dd3606fe5bf1786509387c55f2f05d2894
Ayomide Adedeji <diamonditadedeji@gmail.com>
2024-04-03 12:41:11 +0100

    Refactor lecturerAuthRoutes.js to include getAllLecturers route

--- src/controllers/AuthController/LecturerController.js
+++ src/controllers/AuthController/LecturerController.js
@@ -1,84 +1,115 @@
-const jwt = require('jsonwebtoken');
-const createError = require('http-errors')
-const Lecturer = require('../../models/LecturerModel')
-const { authSchema, loginSchema } = require('../../helpers/validation_schema')
-const { signAccessToken, signRefreshToken, logoutToken } = require('../../helpers/jwt_helper')
-const { Config } = require('../../config/config')
+const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
+const Lecturer = require("../../models/LecturerModel");
+const { authSchema, loginSchema } = require("../../helpers/validation_schema");
+const {
+  signAccessToken,
+  signRefreshToken,
+  logoutToken,
+} = require("../../helpers/jwt_helper");
+const { Config } = require("../../config/config");
 
-const registerLecturer = async (request,response,next) => {
-    try {
-        const result = await authSchema.validateAsync(request.body)
-        const doesExist = await Lecturer.findOne({ email: result.email})
-        if(doesExist)
-            throw createError.Conflict(`${result.email} already exist`)
-        const lecturer = new Lecturer(result)
-        const savedLecturer = await lecturer.save()
-        if (!Config.Jwt_secret) {
-            console.error('Jwt_secret is not defined!');
-            res.status(500).json({ message: 'Internal Server Error' });
-            return;
-        }
-        // Generate a JWT token for the newly registered Admin
-        const token = jwt.sign({ lecturerId: savedLecturer._id, email: savedLecturer.email, fullname: savedLecturer.fullname }, Config.Jwt_secret);
-        response.status(201).json({lecturer: savedLecturer, token})
-    } catch (error) {
-        if(error.isJoi === true) error.status = 422
-        next(error)
+const registerLecturer = async (request, response, next) => {
+  try {
+    const result = await authSchema.validateAsync(request.body);
+    const doesExist = await Lecturer.findOne({ email: result.email });
+    if (doesExist) throw createError.Conflict(`${result.email} already exist`);
+    const lecturer = new Lecturer(result);
+    const savedLecturer = await lecturer.save();
+    if (!Config.Jwt_secret) {
+      console.error("Jwt_secret is not defined!");
+      res.status(500).json({ message: "Internal Server Error" });
+      return;
     }
-}
+    // Generate a JWT token for the newly registered Admin
+    const token = jwt.sign(
+      {
+        lecturerId: savedLecturer._id,
+        email: savedLecturer.email,
+        fullname: savedLecturer.fullname,
+      },
+      Config.Jwt_secret
+    );
+    response.status(201).json({ lecturer: savedLecturer, token });
+  } catch (error) {
+    if (error.isJoi === true) error.status = 422;
+    next(error);
+  }
+};
 
-const loginLecturer = async (request,response,next) => {
-    try {
-        const result = await loginSchema.validateAsync(request.body)
-        const lecturer = await Lecturer.findOne({ email: result.email})
-        if(!lecturer)
-            throw createError.NotFound(`Lecturer Not Found`)
-        const isMatch = await lecturer.isValidPassword(result.password)
-        if(!isMatch) throw createError.Unauthorized('Invalid Email/Password')
-        if (!Config.Jwt_secret) {
-            console.error('Jwt_secret is not defined!');
-            res.status(500).json({ message: 'Internal Server Error' });
-            return;
-        }
-        // Generate a JWT token for the newly registered Admin
-        const token = jwt.sign({ lecturerId: lecturer._id, email: lecturer.email, fullname: lecturer.fullname }, Config.Jwt_secret);
-        response.status(201).json({lecturer: lecturer, token})
-    } catch (error) {
-        if(error.isJoi === true) return next(createError.BadRequest('Invalid Email/Password'))
-        next(error)
+const loginLecturer = async (request, response, next) => {
+  try {
+    const result = await loginSchema.validateAsync(request.body);
+    const lecturer = await Lecturer.findOne({ email: result.email });
+    if (!lecturer) throw createError.NotFound(`Lecturer Not Found`);
+    const isMatch = await lecturer.isValidPassword(result.password);
+    if (!isMatch) throw createError.Unauthorized("Invalid Email/Password");
+    if (!Config.Jwt_secret) {
+      console.error("Jwt_secret is not defined!");
+      res.status(500).json({ message: "Internal Server Error" });
+      return;
     }
-}
+    // Generate a JWT token for the newly registered Admin
+    const token = jwt.sign(
+      {
+        lecturerId: lecturer._id,
+        email: lecturer.email,
+        fullname: lecturer.fullname,
+      },
+      Config.Jwt_secret
+    );
+    response.status(201).json({ lecturer: lecturer, token });
+  } catch (error) {
+    if (error.isJoi === true)
+      return next(createError.BadRequest("Invalid Email/Password"));
+    next(error);
+  }
+};
 
-const logoutLecturer = async (request,response,next) => {
-    try {
-        if(!request.headers['authorization']) next(createError.Unauthorized('Unauthorized Lecturer'))
-        const authHeader = request.headers['authorization']
-        const bearerToken = authHeader.split(' ')
-        const token = bearerToken[1]
-        if (!token) return response.status(400).send('Token not provided');
+const logoutLecturer = async (request, response, next) => {
+  try {
+    if (!request.headers["authorization"])
+      next(createError.Unauthorized("Unauthorized Lecturer"));
+    const authHeader = request.headers["authorization"];
+    const bearerToken = authHeader.split(" ");
+    const token = bearerToken[1];
+    if (!token) return response.status(400).send("Token not provided");
 
-        // Add token to blacklist
-        logoutToken(token);
+    // Add token to blacklist
+    logoutToken(token);
 
-        response.send('Logged out successfully');
-      } catch (error) {
-        // Handle any potential errors that may occur during the logout process.
-        console.error('Error during logout:', error);
-        response.status(500).json({ message: 'Internal Server Error' });
-      }
-}
+    response.send("Logged out successfully");
+  } catch (error) {
+    // Handle any potential errors that may occur during the logout process.
+    console.error("Error during logout:", error);
+    response.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 const profileLecturer = async (request, response, next) => {
-    try {
-        const userId = request.user.lecturerId
-        const lecturer = await Lecturer.findOne({ _id: userId})
-        if(!lecturer)
-            throw createError.NotFound(`Lecturer Not Found`)
-        response.status(200).json({lecturer: lecturer})
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    const userId = request.user.lecturerId;
+    const lecturer = await Lecturer.findOne({ _id: userId });
+    if (!lecturer) throw createError.NotFound(`Lecturer Not Found`);
+    response.status(200).json({ lecturer: lecturer });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+const getAllLecturers = async (request, response, next) => {
+  try {
+    const lecturers = await Lecturer.find();
+    response.status(200).json({ lecturers });
+  } catch (error) {
+    next(error);
+  }
+};
 
-module.exports = { registerLecturer, loginLecturer,logoutLecturer, profileLecturer }
+module.exports = {
+  registerLecturer,
+  loginLecturer,
+  logoutLecturer,
+  profileLecturer,
+  getAllLecturers,
+};
--- src/controllers/StudentController/studentSettingsController.js
+++ src/controllers/StudentController/studentSettingsController.js
@@ -17,7 +17,7 @@ const { Config } = require("../../config/config");
 const fs = require("fs");
 
 async function changePassword(request, response) {
-  const { old_password, new_password, confirm_new_password } = request.body;
+  const { old_password, new_password } = request.body;
   const student_id = request.user.studentId;
   if (!student_id) {
     return response.status(403).json({ message: "Unauthorized User" });
@@ -26,11 +26,8 @@ async function changePassword(request, response) {
     const validationRules = [
       body("old_password").notEmpty().withMessage("Old Password is required"),
       body("new_password")
-        .isLength({ min: 10 })
-        .withMessage("Password must be at least 16 characters long"),
-      body("confirm_new_password")
-        .isLength({ min: 10 })
-        .withMessage("Confirm Password must be at least 16 characters long"),
+        .isLength({ min: 8 })
+        .withMessage("Password must be at least 8 characters long"),
     ];
 
     // Apply validation rules to the request
@@ -40,44 +37,38 @@ async function changePassword(request, response) {
     if (!errors.isEmpty()) {
       return response.status(400).json({ errors: errors.array() });
     }
-=======
-    const { old_password, new_password } = request.body;
-    const student_id = request.user.studentId
-    if (!student_id) {
-        return response.status(403).json({ message: 'Unauthorized User' });
+
+    const student = await Student.findOne({ _id: student_id });
+    if (!student) {
+      return response.status(404).json({ message: "Student Not Found" });
     }
-    try {
-        const validationRules = [
-            body('old_password').notEmpty().withMessage('Old Password is required'),
-            body('new_password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),
-        ];
-        
-        // Apply validation rules to the request
-        await Promise.all(validationRules.map(rule => rule.run(request)));
-        
-        const errors = validationResult(request);
-        if (!errors.isEmpty()) {
-        return response.status(400).json({ errors: errors.array() });
-        }
+    const password = student.password;
 
+    if (password !== null) {
+      // Verify the password
+      const passwordMatch = await bcrypt.compare(old_password, password);
 
-    if (new_password !== confirm_new_password) {
-      return response.status(401).json({ error: "Password Mismatch" });
-=======
-            if (!passwordMatch) {
-                return response.status(401).json({ error: 'Old Password Mismatch' });
-            }
-        }
-
-        // Hash the password before storing it
-        const hashedPassword = await bcrypt.hash(new_password, 10);
-        const update = await Student.findOneAndUpdate( {_id: student_id},{password: hashedPassword}, {new:true})
-    
-        return response.status(200).json({ message:"Password Updated Successfully", data:update})
-    } catch (error) {
-        console.log(error);
-        return response.status(500).json({message:"Internal Server Error"})
+      if (!passwordMatch) {
+        return response.status(401).json({ error: "Old Password Mismatch" });
+      }
     }
+
+    // Hash the password before storing it
+    const hashedPassword = await bcrypt.hash(new_password, 10);
+    const update = await Student.findOneAndUpdate(
+      { _id: student_id },
+      { password: hashedPassword },
+      { new: true }
+    );
+
+    return response
+      .status(200)
+      .json({ message: "Password Updated Successfully", data: update });
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({ message: "Internal Server Error" });
+  }
+}
 
 async function changeProfileImage(request, response) {
   const student_id = request.user.studentId;
