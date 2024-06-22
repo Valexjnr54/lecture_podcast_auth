c33a2b81345ecaaedd59b5fe67febafa298f0a8a
Ayomide Adedeji <diamonditadedeji@gmail.com>
2024-06-22 14:49:21 +0100

    Added services.json

--- src/services/notification.js
+++ src/services/notification.js
@@ -1,6 +1,7 @@
 const admin = require("firebase-admin");
+const serviceAccount = require("../../service.json");
 
-const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+// const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
